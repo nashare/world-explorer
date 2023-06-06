f83300936b98258af4b00959f21be15c7ddc9da9
nashare <tatiana.lavr@gmail.com>
2023-06-06 15:34:51 -0700

    Django Auth

--- countries/settings.py
+++ countries/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
